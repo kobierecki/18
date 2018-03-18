a5a711bf6d4acad1b3c3e9360ce2a96bea1b3a12
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2018-03-18 08:06:31 +0100

    18

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+	new HtmlWebpackPlugin({
+		template: 'src/index.html',
+		filename: 'index.html',
+		inject: 'body',
+	})
+];
+	
+console.log('NODE_ENV:', env);
+	
+if (env === 'production') {
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+		new OptimizeJsPlugin({
+		sourceMap: false
+		})
+	);
+}	
+
+//webpack.config.js
+module.exports = {
+	entry: (env !== 'production' ? [
+		'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server',
+	] : []).concat(['./client/index.js']),
+	output: {
+		filename: './bundle.js',
+		path: path.resolve(__dirname, 'public'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|bower_components)/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader'},
+					{
+						loader: 'css-loader',
+						options: {
+								modules: true
+						}
+					}
+				]
+			}
+		],
+	},
+	plugins
+};
